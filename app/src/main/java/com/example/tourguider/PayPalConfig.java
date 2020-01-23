bdfaf511c0245d2354d30388a3396c72135c63c8
Jakub Zięba <zieba.jakub.97@gmail.com>
2020-01-23 20:09:57 +0100

    ADD PayPal config file - please look at README.md

---
+++ app/src/main/java/com/example/tourguider/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.example.tourguider;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "ENTER_THERE_YOUR_PAYPAL_CLIENT_ID";		//ID removed before pushing file to git.
+}
